4369a871b2b854289c288e238c242820dd958c89
cherneha <staceychernega@gmail.com>
2017-06-16 10:21:10 +0300

    completed network

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 void showNums(vector <double> numRead);
 double countWij(int i, int j, vector <vector <double>> &v);
+double newValueInNum(vector <vector <double>> &weights, vector <double> &num, int p);
 
 int main() {
 
-    vector <vector <double>> patterns(10);
+    vector <vector <double>> patterns(4);
 
     ifstream ofs("/home/stasey/Документы/representValues/number.txt", ofstream::in);
 
@@ -35,8 +36,38 @@ int main() {
     for(int i = 0; i < weights.size(); i++){
         for(int j = 0; j < weights[i].size(); j++){
             weights[i][j] = countWij(i, j, patterns);
+            if(i == j){
+                weights[i][j] = 0;
+            }
         }
     }
+
+    vector <double> num;
+
+    ofs.close();
+    ofs.open("/home/stasey/Документы/representValues/4.txt", ofstream::in);
+    while (ofs >> a){
+        num.push_back(a);
+    }
+
+    showNums(num);
+
+
+
+    bool changed = false;
+    while(!changed){
+        cout << "iter" << endl;
+        changed = true;
+        for(int i = num.size(); i >= 0; i--){
+            double newVal = newValueInNum(weights, num, i);
+            if(newVal != num[i]){
+                changed = false;
+                num[i] = newVal;
+            }
+        }
+    }
+
+    showNums(num);
 
     return 0;
 }
@@ -63,4 +94,21 @@ double countWij(int i, int j, vector <vector <double>> &v){
         sum = sum + (2*v[s][i] - 1)*(2*v[s][j] - 1);
     }
     return sum;
+}
+
+// updating node value
+
+double newValueInNum(vector <vector <double>> &weights, vector <double> &num, int p){
+    double newValue = 0;
+    for(int i = 0; i < num.size(); i++){
+        if(i != p){
+            newValue = newValue + weights[i][p]*num[i];
+        }
+    }
+    if(newValue >= 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
